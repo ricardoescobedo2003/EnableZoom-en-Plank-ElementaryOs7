c665887ed5457a42bbb1619a4eb5db6d963bd57e
Ricardo Escobedo <160807698+ricardoescobedo2003@users.noreply.github.com>
2024-05-19 22:15:36 +0000

    Script Bash
    Script para habilitar el zoom de plank en elementry os 7.1

---
+++ enableZoom.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+wget https://launchpadlibrarian.net/721328246/plank_0.11.89.orig.tar.xz
+tar xf plank_0.11.89.orig.tar.xz
+sudo apt build-dep plank -y
+sudo apt install --reinstall plank libplank1 libplank-common libplank-dev libplank-doc -y
+sudi apt install figlet -y
+cd plank-0.11.89
+./configure
+sudo make install
+sudo rm /usr/share/plank/themes/Default/dock.theme
+cd ..
+sudo cp dock.theme /usr/share/plank/themes/Default/
+sudo rm /usr/share/glib-2.0/schemas/net.launchpad.plank.gschema.xml
+sudo cp net.launchpad.plank.gschema.xml /usr/share/glib-2.0/schemas/
+killall plank
+plank
+figlet REINICIA TU MAQUINA
