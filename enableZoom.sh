304d8e93199728b3389c11175eb20f10b979f95b
Ricardo Escobedo <160807698+ricardoescobedo2003@users.noreply.github.com>
2024-05-19 16:25:11 -0600

    bug

--- enableZoom.sh
+++ enableZoom.sh
@@ -3,7 +3,7 @@ wget https://launchpadlibrarian.net/721328246/plank_0.11.89.orig.tar.xz
 tar xf plank_0.11.89.orig.tar.xz
 sudo apt build-dep plank -y
 sudo apt install --reinstall plank libplank1 libplank-common libplank-dev libplank-doc -y
-sudi apt install figlet -y
+sudo  apt install figlet -y
 cd plank-0.11.89
 ./configure
 sudo make install
@@ -13,5 +13,5 @@ sudo cp dock.theme /usr/share/plank/themes/Default/
 sudo rm /usr/share/glib-2.0/schemas/net.launchpad.plank.gschema.xml
 sudo cp net.launchpad.plank.gschema.xml /usr/share/glib-2.0/schemas/
 killall plank
+figlet REINICIA TU MAQUINA - Aplica los cambios
 plank
-figlet REINICIA TU MAQUINA
